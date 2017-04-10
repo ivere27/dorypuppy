919d2dc7b7c3e1a8e214d4aa4e0ecc2f537efc93
yONg <ivere27@gmail.com>
2017-04-11 08:45:56 +0900

    add uv headers(v1.11.0) and SimpleProcessSpawn example

--- SampleApplication/app/src/main/cpp/native-lib.cpp
+++ SampleApplication/app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,15 @@
 #include <jni.h>
 #include <string>
 #include <cinttypes>
+#include <iostream>
 
 #include <android/log.h>
 #include "uv.h"
+
+#include "SimpleProcessSpawn.hpp"
+
+using namespace std;
+using namespace spawn;
 
 #define LOGI(...) \
   ((void)__android_log_print(ANDROID_LOG_INFO, "dorypuppy::", __VA_ARGS__))
@@ -14,11 +20,38 @@ Java_io_tempage_dorypuppy_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
 
+    std::string hello = "Hello from C++";
+
     int err;
     double uptime;
     err = uv_uptime(&uptime);
     LOGI("uv_uptime: %" PRIu64, uptime);
 
-    std::string hello = "Hello from C++";
+    {
+        uv_loop_t *uv_loop = uv_default_loop();
+        char *args[3];
+        args[0] = (char *) "/system/bin/ls";
+        args[1] = (char *) "/";
+        args[2] = NULL;
+
+        SimpleProcessSpawn process(uv_loop, args);
+        process.timeout = 1000;
+        process.on("error", [](Error &&error) {
+            cout << error.name << endl;
+            cout << error.message << endl;
+        })
+        .on("response", [&](Response &&response) {
+            cout << "exit code : " << response.exitStatus << endl;
+            cout << "signal : " << response.termSignal << endl;
+            cout << response.out.str();
+            cout << response.err.str();
+
+            hello = response.out.str();
+        })
+        .spawn();
+
+        uv_run(uv_loop, UV_RUN_DEFAULT);
+    }
+
     return env->NewStringUTF(hello.c_str());
 }
