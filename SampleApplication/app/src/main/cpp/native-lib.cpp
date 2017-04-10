db7244baafc8f399f53fff5326767859c8f783ec
yONg <ivere27@gmail.com>
2017-04-11 08:26:03 +0900

    add 'libuv.a' to sample app

--- SampleApplication/app/src/main/cpp/native-lib.cpp
+++ SampleApplication/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <cinttypes>
+
+#include <android/log.h>
+#include "uv.h"
+
+#define LOGI(...) \
+  ((void)__android_log_print(ANDROID_LOG_INFO, "dorypuppy::", __VA_ARGS__))
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_io_tempage_dorypuppy_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    int err;
+    double uptime;
+    err = uv_uptime(&uptime);
+    LOGI("uv_uptime: %" PRIu64, uptime);
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
