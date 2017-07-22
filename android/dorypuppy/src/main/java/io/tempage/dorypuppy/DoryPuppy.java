0892a66a65e027baab3bebf5317d73a58327f188
yONg <ivere27@gmail.com>
2017-07-22 14:31:43 +0900

    bug fix. process object can not be deleted before to be referenced

--- android/dorypuppy/src/main/java/io/tempage/dorypuppy/DoryPuppy.java
+++ android/dorypuppy/src/main/java/io/tempage/dorypuppy/DoryPuppy.java
@@ -48,11 +48,12 @@ public class DoryPuppy {
     }
     private void jniExit(int pid, long code, int signal) {
         //Log.i("dorypuppy", pid + " : " + code + " / " + signal);
-        processList.remove(pid);
 
         if (processList.containsKey(pid) && processList.get(pid).exitListener != null) {
             processList.get(pid).exitListener.listener(code, signal);
         }
+
+        processList.remove(pid);
     }
 
 
